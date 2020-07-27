5f46ca43edd2d16e907ac1bc9208fb404b21a9c6
Rahul Thapa <rahulthapa.work@gmail.com>
2020-07-27 18:27:54 +0530

    Fixed error due to proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,4 +1,4 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
-// module.exports = function (app) {
-//   app.use(createProxyMiddleware("/api", { target: "http://localhost:5000" }));
-// };
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function (app) {
+  app.use(createProxyMiddleware("/api", { target: "http://localhost:5000" }));
+};
